d70c19d8f93f9753b922efbedcece043cfc7dd80
vi.latyshev <storis2012@yandex.ru>
2021-06-09 16:56:41 +0300

    fix: improve fetch stories parser [wip]

--- app/api/routes/inst/stories.py
+++ app/api/routes/inst/stories.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Body, Depends, HTTPException
 
 from plugins.instagram.instagram_client import api as instagram_api
-from plugins.instagram.parser import (fetch_stories, stories_count)
+from plugins.instagram.parser import (fetch_stories, fetch_stories_count, fetch_one_story_by_index)
 
 router = APIRouter()
 
@@ -12,14 +12,13 @@ def fetch_all_stories(username: str):
 
 @router.get('/{username}/count')
 def count_stories(username: str):
-    return {'data': stories_count(instagram_api, username)}
+    return {'data': fetch_stories_count(instagram_api, username)}
 
 @router.get('/{username}/{index_story}')
 def fetch_one_story(username: str, index_story: int):
+    story = fetch_one_story_by_index(username, index_story)
 
-    stories = fetch_stories(instagram_api, username)
-    stories_count = len(stories)
-    if stories_count < index_story:
+    if (story == None):
         return HTTPException(404)
     else:
-        return {'data': stories[index_story - 1]}
+        return {'data': story}
--- app/plugins/instagram/parser.py
+++ app/plugins/instagram/parser.py
@@ -1,15 +1,40 @@
 import instagram_private_api
 
+def stories_raw_to_object(stories: dict) -> dict:
+    story_object = {}
+
+    # optional params
+    if 'audience' in story_dict:
+        story_object['audience'] = story_dict['audience']
+
+    if 'imported_taken_at' in story_dict:
+        story_object['original_created_at'] = story_dict['imported_taken_at']
+
+    story_object['type'] = story_dict['media_type']
+
+    story_object['height'] = story_dict['original_height']
+    story_object['width'] = story_dict['original_width']
+
+    story_object['image_url'] = story_dict['image_versions2']['candidates'][0]['url']
+    story_object['preview_url'] = story_dict['image_versions2']['candidates'][1]['url']
+
+    story_object['id'] = story_dict['pk']
+
+    story_object['created_at'] = story_dict['taken_at']
+
+    return story_object
+
 
 def username_to_pk(api: instagram_private_api.client.Client, username: str) -> int: # getting pk(id) of instagram account from username
     search_results = api.username_info(username)
     search_id = search_results['user']['pk']
     return search_id
 
+
 def pk_to_username(api: instagram_private_api.client.Client, pk: int) -> str: # instagram id(pk) to instagram username
     search_results = api.user_info(pk)
     username = search_results['user']['username']
-    
+
     return username
 
 
@@ -18,7 +43,7 @@ def fetch_following_pk(api: instagram_private_api.client.Client, pk: int) -> lis
     uuid = api.generate_uuid()
     results = api.user_following(pk, uuid)
     followers = results['users']
-    
+
     list_acconts_pk = []
     for human in followers:
         list_acconts_pk.append(human['pk'])
@@ -26,43 +51,41 @@ def fetch_following_pk(api: instagram_private_api.client.Client, pk: int) -> lis
     return list_acconts_pk
 
 
-def fetch_stories(api: instagram_private_api.client.Client, username: str) -> list[dict]: # fetching list of user's stories
-
+def get_stories_raw(api: instagram_private_api.client.Client, username: str) -> list[dict]:
     user_pk = username_to_pk(api, username)
 
-    list_of_stories = []
     stories = api.user_story_feed(user_pk)
-    if stories['reel']:
-        list_stories_full = stories['reel']['items']
 
-        for i in range(len(list_stories_full)):
-            
-            story_dict = list_stories_full[i]
-            story_object = {}
+    if stories['reel'] == None:
+        return []
 
-            # optional params
-            if 'audience' in story_dict:
-                story_object['audience'] = story_dict['audience']
-                        
-            if 'imported_taken_at' in story_dict:
-                story_object['original_created_at'] = story_dict['imported_taken_at']
-    
-            story_object['type'] = story_dict['media_type'] 
-
-            story_object['height'] = story_dict['original_height']
-            story_object['width'] = story_dict['original_width']
-                
-            story_object['image_url'] = story_dict['image_versions2']['candidates'][0]['url']
-            story_object['preview_url'] = story_dict['image_versions2']['candidates'][1]['url']
-
-            story_object['id'] = story_dict['pk']
-            
-            story_object['created_at'] = story_dict['taken_at']
+    return stories['reel']['items']
 
 
-            list_of_stories.append(story_object)
+def fetch_stories(api: instagram_private_api.client.Client, username: str) -> list[dict] | None:
+
+    stories = get_stories_raw(api, username)
+
+    for i in range(len(stories)):
+        story_object = stories_raw_to_object(stories[i])
+
+        list_of_stories.append(story_object)
 
     return list_of_stories
 
-def stories_count(api: instagram_private_api.client.Client, username: str) -> int:
-    return len(fetch_stories(api, username))
+
+def fetch_stories_count(api: instagram_private_api.client.Client, username: str) -> int:
+    return len(get_stories_raw(api, username))
+
+
+def fetch_one_story_by_index(api: instagram_private_api.client.Client, username: str, index: int) -> list[dict]:
+    stories = get_stories_raw(api, username)
+
+    story_by_index = list_stories_full[index - 1]
+
+    if (story_by_index == None):
+        return None
+
+    story_object = stories_raw_to_object(stories[i])
+
+    return story_object
