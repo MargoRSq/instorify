fb2ebd97da8288d6e70d7914b35cfba356c36950
MargoRS <slava1kvartovkin@gmail.com>
2021-06-13 18:30:27 +0300

    fix: auth try-except model

--- app/plugins/instagram/clients/private_api.py
+++ app/plugins/instagram/clients/private_api.py
@@ -2,11 +2,11 @@ import json
 import os
 
 from plugins.instagram.clients.utils import (COOCKIE_PATH_PRIVATE, LOGIN,
-                                                 PASS, from_json, handle_login)
+                                                 PASS, from_json, handle_login, handle_login_refresh)
 
 from instagram_private_api import (Client, ClientCookieExpiredError,
                                    ClientError, ClientLoginError,
-                                   ClientLoginRequiredError)
+                                   ClientLoginRequiredError, ClientThrottledError)
 
 def auth_without_settings():
     return Client(
@@ -14,32 +14,52 @@ def auth_without_settings():
                 password=PASS, 
                 on_login=lambda x: handle_login(x, COOCKIE_PATH_PRIVATE))
 
-
 def auth_with_settings(settings):
     return Client(
                 username=LOGIN, 
                 password=PASS, 
                 settings=settings) 
 
-      
-try:
+def auth_refresh():
+    os.remove(COOCKIE_PATH_PRIVATE)
+    return Client(
+                username=LOGIN, 
+                password=PASS, 
+                on_login=lambda x: handle_login_refresh(x, COOCKIE_PATH_PRIVATE))
 
-    if not os.path.isfile(COOCKIE_PATH_PRIVATE):
+def auth():
+    try:
+        if not os.path.isfile(COOCKIE_PATH_PRIVATE):
+            # If cookies exists
+            private_api = auth_without_settings()
+        else:
+            # Create cookies
+            with open(COOCKIE_PATH_PRIVATE) as file_data:
+                cached_settings_private = json.load(file_data, object_hook=from_json)
+                # reuse auth settings
+                private_api = auth_with_settings(cached_settings_private)
+            
+    except (ClientCookieExpiredError, ClientLoginRequiredError) as e:
+        print(e)
+        # Login expired
         private_api = auth_without_settings()
-    else:
-        with open(COOCKIE_PATH_PRIVATE) as file_data:
-            cached_settings_private = json.load(file_data, object_hook=from_json)
-            # reuse auth settings
-            private_api = auth_with_settings(cached_settings_private)
-        
-except (ClientCookieExpiredError, ClientLoginRequiredError) as e:
-    # Login expired
-    private_api = auth_without_settings()
+    except ClientThrottledError as e:
+        print(e)
+        # Please wait a few minutes before you try again
+        private_api = auth_refresh()
+    except ClientLoginError as e:
+        print(e)
+        # Raised when login fails
+        private_api = auth_without_settings()
+    except ClientError as e:
+        print(e)
+        # Other login errors
+        private_api = auth_without_settings()
+    except Exception as e:
+        print(e)
+        exit()
 
-except ClientLoginError as e:
-    private_api = auth_without_settings()
-except ClientError as e:
-    private_api = auth_without_settings()
-except Exception as e:
-    print(e)
-    private_api = auth_without_settings()
+    return private_api
+
+      
+private_api = auth()
--- app/plugins/instagram/clients/web_api.py
+++ app/plugins/instagram/clients/web_api.py
@@ -6,13 +6,13 @@ import random
 import string
 
 import instagram_web_api
-
-from instagram_private_api import (Client, ClientCookieExpiredError,
-                                   ClientError, ClientLoginError,
-                                   ClientLoginRequiredError)
+from instagram_web_api import (ClientCookieExpiredError, ClientError,
+                               ClientLoginError,
+                               ClientThrottledError)
 
 from plugins.instagram.clients.utils import (COOCKIE_PATH_WEB, LOGIN, PASS,
-                                                 from_json, handle_login)
+                                             from_json, handle_login)
+
 
 class WebApiClient(instagram_web_api.Client):
 
@@ -34,7 +34,11 @@ class WebApiClient(instagram_web_api.Client):
         time = str(int(datetime.datetime.now().timestamp()))
         enc_password = f"#PWD_INSTAGRAM_BROWSER:0:{time}:{self.password}"
 
-        params = {'username': self.username, 'enc_password': enc_password, 'queryParams': '{}', 'optIntoOneTap': False}
+        params = {
+                'username': self.username, 
+                'enc_password': enc_password, 
+                'queryParams': '{}', 
+                'optIntoOneTap': False}
         self._init_rollout_hash()
         
         login_res = self._make_request('https://www.instagram.com/accounts/login/ajax/', params=params)
@@ -42,10 +46,10 @@ class WebApiClient(instagram_web_api.Client):
             raise ClientLoginError('Unable to login')
 
         if self.on_login:
-            on_login_callback = self.on_login
-            on_login_callback(self)
+            on_login_callback = self.on_login(self)
         
         return login_res
+
 
 def auth_without_settings():
     return WebApiClient(
@@ -53,31 +57,45 @@ def auth_without_settings():
                 password=PASS, 
                 on_login=lambda x: handle_login(x, COOCKIE_PATH_WEB))
 
-
 def auth_with_settings(settings):
     return WebApiClient(
                 username=LOGIN, 
                 password=PASS, 
                 settings=settings)          
 
-try:
-
-    if not os.path.isfile(COOCKIE_PATH_WEB):
+def auth():
+    try:
+        if not os.path.isfile(COOCKIE_PATH_WEB):
+            # If cookies exists
+            web_api = auth_without_settings()
+        else:
+            # Create cookies
+            with open(COOCKIE_PATH_WEB) as file_data:
+                cached_settings_private = json.load(file_data, object_hook=from_json)
+                # reuse auth settings
+                web_api = auth_with_settings(cached_settings_private)
+            
+    except (ClientCookieExpiredError) as e:
+        print(e)
+        # Login expired
         web_api = auth_without_settings()
-    else:  
-        with open(COOCKIE_PATH_WEB) as file_data:
-            cached_settings_web = json.load(file_data, object_hook=from_json)
-            # reuse auth settings
-            web_api = auth_with_settings(cached_settings_web)
-        
-except (ClientCookieExpiredError, ClientLoginRequiredError) as e:
-    # Login expired
-    web_api = auth_without_settings()
+    except ClientThrottledError as e:
+        print(e)
+        # Please wait a few minutes before you try again
+        web_api = auth_without_settings()
+    except ClientLoginError as e:
+        print(e)
+        # Raised when login fails
+        web_api = auth_without_settings()
+    except ClientError as e:
+        print(e)
+        # Other login errors
+        web_api = auth_without_settings()
+    except Exception as e:
+        print(e)
+        exit()
+    
+    return web_api
 
-except ClientLoginError as e:
-    web_api = auth_without_settings()
-except ClientError as e:
-    web_api = auth_without_settings()
-except Exception as e:
-    print(e)
-    web_api = auth_without_settings()
+
+web_api = auth()
