4e6d525071edd2696700abdbf67f7d44dd59e527
MargoRS <slava1kvartovkin@gmail.com>
2021-06-20 17:29:56 +0300

    feat: CORS [WIP]

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh -c 'cd ./directory/ && uvicorn main:app'
--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes.api import router as api_router
 
+origins = ['http://localhost:3000']
 
 def get_application() -> FastAPI:
     application = FastAPI(title='api')
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+    )
 
     application.include_router(api_router)
 
