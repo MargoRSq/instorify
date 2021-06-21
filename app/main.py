de13a4064bbe5cbf279290e88ac684340d440309
MargoRSq <49024821+MargoRSq@users.noreply.github.com>
2021-06-21 12:49:28 +0300

    feat: CORS (#11)
    * feat: CORS [WIP]
    
    * fix: Procfile cd dir
    
    * fix: uvicorn for linux
    
    * fix: run without cd
    
    * fix: Pipfile
    
    * fix: Pipfile and .lock
    
    * fix: Procfile
    
    * chore: tmp
    
    * fix: creating cache dir
    
    * fix: regex
    
    * fix: Pipfile and Procfile
    
    * fix: Pipfile.lock

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes.api import router as api_router
 
+origins_regex = r'http(s?)://localhost:3000'
 
 def get_application() -> FastAPI:
     application = FastAPI(title='api')
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origin_regex=origins_regex,
+        allow_credentials=True,
+        allow_methods=["*"],
+    )
 
     application.include_router(api_router)
 
