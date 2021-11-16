a73858deb4c93289a3dfaf57840a41be8e3e31f6
MargoRSq <slava1kvartovkin@gmail.com>
2021-11-16 10:31:04 +0300

    fix: exceptions structure and comments in config

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,14 @@
 import os
 from starlette.config import Config
 
+# FastAPI instance data
 API_DOCS_URL = "/__docs"
 API_REDOC_URL = None
 
 API_VERSION = "0.1.1"
-
 API_PROJECT_NAME = "instorify-api"
 
+# in case of failed auth on instagram
 PLUGINS_ACCOUNTS_MAX_RETRY = 5
 PLUGINS_ACCOUNTS_COOKIE_PATH = os.path.join(os.getcwd(), 'cache')
 
--- app/main.py
+++ app/main.py
@@ -1,21 +1,11 @@
-import uvicorn
-import aioredis
-import sys
-import asyncio
-
-from fastapi import FastAPI, applications
-from fastapi.exception_handlers import (http_exception_handler,
-                                        request_validation_exception_handler)
-from fastapi.exceptions import RequestValidationError
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 # from fastapi_cache import FastAPICache
 # from fastapi_cache.backends.redis import RedisBackend
-from instagram_private_api.errors import ClientError
-from starlette.exceptions import HTTPException as StarletteHTTPException
-from starlette.responses import JSONResponse
 
 from app.api.routes.api import router as api_router
 from app.core.config import API_DOCS_URL, API_PROJECT_NAME, REDIS_URL, API_REDOC_URL, API_VERSION, API_DEBUG, API_RELOAD
+from app.api.errors.exceptions_handlers import subscribe_exception_handlers
 
 origins_regex = r'http(s?)://localhost:3000'
 
@@ -38,18 +28,9 @@ def get_application() -> FastAPI:
     #     redis = await aioredis.create_redis_pool(REDIS_URL, encoding="utf8")
     #     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
 
-    @application.exception_handler(ClientError)
-    async def custom_http_exception_handler(request, exc):
-        if exc.msg == 'Not Found: user_not_found':
-            return JSONResponse({'detail': 'user not found'}, status_code=404)
+    subscribe_exception_handlers(application)
 
-    @application.exception_handler(StarletteHTTPException)
-    async def custom_http_exception_handler(request, exc):
-        return await http_exception_handler(request, exc)
-
-    @application.exception_handler(RequestValidationError)
-    async def validation_exception_handler(request, exc):
-        return await request_validation_exception_handler(request, exc)
     return application
+
 
 app = get_application()
